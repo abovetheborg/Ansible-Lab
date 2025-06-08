862737d0a1e322f1320a09ebc59985c2ad0093d9
Michel JACQUES <michel.jacques@polymtl.ca>
2025-06-08 22:45:33 +0000

    fix: replace printf with here-doc syntax for YAML file creation in tutorial.bash

--- tutorial.bash
+++ tutorial.bash
@@ -3,10 +3,28 @@
 ansible-galaxy collection init foo.bar
 cd foo/bar/roles
 ansible-galaxy role init my_role
-printf -- '--- \n- name: Task is running from within the role\n  ansible.builtin.debug:\n    msg: "This is a task from my_role."' > my_role/tasks/main.yml
+#printf -- '--- \n- name: Task is running from within the role\n  ansible.builtin.debug:\n    msg: "This is a task from my_role."' > my_role/tasks/main.yml
+cat << EOF > my_role/tasks/main.yml
+---
+- name: Task is running from within the role
+  ansible.builtin.debug:
+    msg: "This is a task from my_role."
+EOF
+
 cd ..
 mkdir -p playbooks
-printf -- '---\n- name: Test new role from within this playbook\n  hosts: localhost\n  gather_facts: false\n  tasks:\n    - name: Testing role\n      ansible.builtin.include_role:\n        name: foo.bar.my_role\n        tasks_from: main.yml' > playbooks/my_playbook.yml
+#printf -- '---\n- name: Test new role from within this playbook\n  hosts: localhost\n  gather_facts: false\n  tasks:\n    - name: Testing role\n      ansible.builtin.include_role:\n        name: foo.bar.my_role\n        tasks_from: main.yml' > playbooks/my_playbook.yml
+cat << EOF > playbooks/my_playbook.yml
+---
+- name: Test new role from within this playbook
+  hosts: localhost
+  gather_facts: false
+  tasks:
+    - name: Testing role
+      ansible.builtin.include_role:
+        name: foo.bar.my_role
+        tasks_from: main.yml
+EOF
 
 mkdir -p extensions
 cd extensions
