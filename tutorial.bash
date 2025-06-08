0c7967a3d51f55bce48322c5bd76a3ea71be4326
Michel JACQUES <michel.jacques@polymtl.ca>
2025-06-08 03:12:58 +0000

    fix: improve formatting in tutorial.bash for better readability

--- tutorial.bash
+++ tutorial.bash
@@ -3,10 +3,10 @@
 ansible-galaxy collection init foo.bar
 cd foo/bar/roles
 ansible-galaxy role init my_role
-printf -- '--- \n- name: Task is running from within the role\n\tansible.builtin.debug:\n\t\tmsg: "This is a task from my_role."' > my_role/tasks/main.yml
+printf -- '--- \n- name: Task is running from within the role\n  ansible.builtin.debug:\n    msg: "This is a task from my_role."' > my_role/tasks/main.yml
 cd ..
 mkdir -p playbooks
-printf -- '---\n- name: Test new role from within this playbook\n\thosts: localhost\n\tgather_facts: false\n\ttasks:\n\t\t- name: Testing role\n\t\t\tansible.builtin.include_role:\n\t\t\t\tname: foo.bar.my_role\n\t\t\t\ttasks_from: main.yml' > playbooks/my_playbook.yml
+printf -- '---\n- name: Test new role from within this playbook\n  hosts: localhost\n  gather_facts: false\n  tasks:\n    - name: Testing role\n      ansible.builtin.include_role:\n        name: foo.bar.my_role\n        tasks_from: main.yml' > playbooks/my_playbook.yml
 
 mkdir -p extensions
 cd extensions
